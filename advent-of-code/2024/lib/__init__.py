6d5657a769dfd29bb7078ab272a6654f0f202553
Guilherme Kunigami <kunigami@fb.com>
2025-05-13 10:06:53 -0700

    p12-hard, lib

---
+++ advent-of-code/2024/lib/__init__.py
@@ -0,0 +1,62 @@
+class Idx: 
+    def __init__(self, i, j):
+        self._idx = (i, j)
+    
+    def __getitem__(self, p):
+        return self._idx[p]
+
+    def __add__(self, o):
+        return Idx(self._idx[0] + o._idx[0], self._idx[1] + o._idx[1])
+
+    def adj4(self):
+        deltas = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        for i, j in deltas:
+            yield self + Idx(i, j)
+
+
+class Mat:
+    def __init__(self, arr):
+        self._m = arr 
+
+    def h(self):
+        return len(self._m)
+
+    def w(self):
+        return len(self._m[0])
+
+    def indices(self):
+        for i in range(self.h()):
+            for j in range(self.w()):
+                yield Idx(i, j)
+
+    def within(self, idx):
+        i, j = idx
+        return i >= 0 and i < self.h() and j >= 0 and j < self.w()
+            
+    def __getitem__(self, idx):
+        i, j = idx
+        return self._m[i][j]
+
+    def __setitem__(self, idx, v):
+        i, j = idx
+        self._m[i][j] = v
+
+    def fill(self, v):
+        for i in range(self.h()):
+            for j in range(self.w()):
+                self._m[i][j] = v
+        
+
+    @staticmethod
+    def new(h, w, value):
+        m = []
+        for _ in range(h):
+            m.append([])
+            for _ in range(w):
+                if callable(value):
+                    v = value()
+                else:
+                    v = value                
+                m[-1].append(v)
+        return Mat(m)
+ 
---
+++ advent-of-code/2024/puzzle-12/solve-hard.py
@@ -0,0 +1,54 @@
+import sys
+from enum import auto, Enum
+from lib import Idx, Mat 
+
+b = Mat([line for line in sys.stdin.read().strip().split("\n")])
+e = Mat.new(b.h(), b.w(), set)
+v = Mat.new(b.w(), b.h(), False)
+
+def populate(idx):
+    if v[idx]:
+        return
+    v[idx] = True
+
+    for i, nidx in enumerate(idx.adj4()):
+        if (not b.within(nidx)) or b[nidx] != b[idx]:
+            e[idx].add(i)
+        else:
+            populate(nidx)
+
+
+def traverse(b, v, idx):
+    if v[idx]:
+        return 0, 0
+    v[idx] = True
+    perimeter = 0
+    area = 1
+    covered = set()
+    for di, dj in [(-1, 0), (0, -1)]:
+        nidx = idx + Idx(di, dj)
+        if b.within(nidx) and b[nidx] == b[idx]:
+            covered = covered | e[nidx]
+
+    for nidx in idx.adj4():        
+        if b.within(nidx) and b[nidx] == b[idx]:
+            p, a = traverse(b, v, nidx)
+            perimeter += p
+            area += a
+    perimeter += len(e[idx] - covered)
+
+    return perimeter, area
+
+for idx in b.indices():
+    if not v[idx]:
+        populate(idx)
+
+v.fill(False)
+t = 0
+for idx in b.indices():
+    if v[idx]:
+        continue
+    p, a = traverse(b, v, idx)
+    t += p * a
+
+print(t)
