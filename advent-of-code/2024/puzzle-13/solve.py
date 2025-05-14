8eebc2100e2afcef7fd5f3a3e17564955aaacdaa
Guilherme Kunigami <kunigami@fb.com>
2025-05-13 21:41:47 -0700

    p13

---
+++ advent-of-code/2024/puzzle-13/solve.py
@@ -0,0 +1,46 @@
+import re
+import sys
+
+input = sys.stdin.read().strip()
+lines = [line for line in input.split("\n")]
+pattern = re.compile(r'(\w+(?: \w+)?): X([+=-]\d+), Y([+=-]?\d+)')
+
+t = 0
+
+def parse_line(line):
+    match = pattern.match(line)
+    assert match
+    _label, x_str, y_str = match.groups()
+    x = int(x_str.replace('=', ''))
+    y = int(y_str.replace('=', ''))
+    return x, y
+
+
+l = (len(lines) + 1)//4
+for i in range(l):
+    l1, l2, l3 = lines[i*4: i*4+3]
+
+    x1, y1 = parse_line(l1)
+    x2, y2 = parse_line(l2)
+    x, y = parse_line(l3)
+
+    x += 10000000000000
+    y += 10000000000000
+
+    best = 123456789
+    found = False
+
+    b1 = x1 * y - y1 * x
+    b2 = x1 * y2 - y1 * x2
+    print(b1, b2, b1 % b2)
+    if b1 % b2 != 0:
+        continue
+    b = b1 // b2
+    a1 = x - b * x2
+    if a1 % x1 != 0:
+        continue
+    a = a1 // x1
+
+    t += 3 * a + b
+
+print(t)
